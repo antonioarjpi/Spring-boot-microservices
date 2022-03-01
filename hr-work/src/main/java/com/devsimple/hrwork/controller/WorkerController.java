4e0922f22b2e07226b2746c4ff3876fbcbb67a61
unknown <antonioarjpi@gmail.com>
2022-03-01 15:33:48 -0300

    Config, bootstrap e actuator

--- hr-work/src/main/java/com/devsimple/hrwork/controller/WorkerController.java
+++ hr-work/src/main/java/com/devsimple/hrwork/controller/WorkerController.java
@@ -2,7 +2,11 @@ package com.devsimple.hrwork.controller;
 
 import com.devsimple.hrwork.model.Worker;
 import com.devsimple.hrwork.services.WorkerService;
-import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
-@AllArgsConstructor
 @RequestMapping("/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
+    @Autowired
     private WorkerService workerService;
 
     @GetMapping
@@ -28,5 +38,11 @@ public class WorkerController {
     public ResponseEntity<Worker> findById(@PathVariable Long id){
         Worker worker = workerService.findById(id);
         return ResponseEntity.ok(worker);
+    }
+
+    @GetMapping("/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }
