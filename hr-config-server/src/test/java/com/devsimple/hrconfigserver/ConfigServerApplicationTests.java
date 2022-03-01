ea4a6aabfb8705260d38f88e1426534081866a99
unknown <antonioarjpi@gmail.com>
2022-03-01 15:01:25 -0300

    Config cloud

---
+++ hr-config-server/src/test/java/com/devsimple/hrconfigserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsimple.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
